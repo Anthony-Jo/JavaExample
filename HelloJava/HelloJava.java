af5396c0a960404ce6f1328c2193db73b1e6179f
AnthonyJo <joseongkwang@gmail.com>
2021-01-01 17:47:16 +0900

    project init

---
+++ HelloJava/HelloJava.java
@@ -0,0 +1,9 @@
+package HelloJava;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("hello Java");
+        int number = 100;
+        System.out.println("a = " + number );
+    }
+}
